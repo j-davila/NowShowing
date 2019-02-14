05b71d15defd887661991ae25e03f9f88b877b8c
Jose <josedavila@joses-mac.local>
2019-02-13 21:51:52 -0800

    1. Added movie details screen. When user clicks a movie tile it will open up another window with the movie poster and the full synopsis. 2. Added logic to connect new view and to get the corresponding data

---
+++ NowShowing/MovieDetailsViewController.swift
@@ -0,0 +1,56 @@
+//
+//  MovieDetailsViewController.swift
+//  NowShowing
+//
+//  Created by Jose  Davila on 2/13/19.
+//  Copyright © 2019 Jose Davila. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var backdropImageView: UIImageView!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    var movie: [String:Any]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+        
+        let baseURL = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseURL + posterPath)
+        
+        posterView.af_setImage(withURL: posterURL!)
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropURL = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
+        
+        backdropImageView.af_setImage(withURL: backdropURL!)
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
