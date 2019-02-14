894f4133a3a6e5c8d1c3de2d1beedbbd9c400f83
Jose <josedavila@joses-mac.local>
2019-02-14 00:11:41 -0800

    1. Added collection view 2. added logic to display superhero movie data from API 3. Added logic display multiple posters per row in collection view

---
+++ NowShowing/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  NowShowing
+//
+//  Created by Jose  Davila on 2/13/19.
+//  Copyright © 2019 Jose Davila. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
